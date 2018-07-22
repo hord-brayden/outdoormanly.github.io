420f0222a484cffbb95c37819305295d8de13134
outdoormanly <35241666+outdoormanly@users.noreply.github.com>
2018-07-22 11:46:42 -0700

    Add files via upload
    Adjusted file extensions to work with GitHub.io instead of usual domain

---
+++ include/nav.php
@@ -0,0 +1,13 @@
+
+<div id='cssmenu'>
+<ul>
+<li class='active'><a href='/'><span>Home</span></a></li>
+<li><a href="/StyleGuide.html"><span>Products</span></a></li>
+<li><a href="/PurposeStatement.html"><span>Purpose Statement</span></a></li>
+<li><a href="/AudienceDescription.html"><span>Audience Description</span></a></li>
+<li><a href="/Personas.html"><span>Personas</span></a></li>
+<li><a href="/Scenarios.html"><span>Scenarios</span></a></li>
+<li class='last'><a href="/ContentList.html"><span>Content List</span></a></li>
+</a></li>
+</ul>
+</div>
